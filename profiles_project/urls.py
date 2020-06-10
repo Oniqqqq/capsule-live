27a714dfa8749414ce9d84a612caa320f5e3d522
Oniqqq <Djagawot@gmail.com>
2020-06-10 11:34:19 +0300

    ad

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^account/', include('allauth.urls')),
     url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
 
