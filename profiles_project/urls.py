07ae9d9b2906c82add871b7adf0c986f80dd08b7
Oniqqq <Djagawot@gmail.com>
2020-06-12 13:05:28 +0300

    aaa

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
-    url(r'^/sendconfirmationemail/', EmailConfirmation.as_view(), name='send-email-confirmation'),
+    url(r'^sendconfirmationemail/', EmailConfirmation.as_view(), name='send-email-confirmation'),
     url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
 
