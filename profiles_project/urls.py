47046e205e465fb281f906ce4eb4e02ca0501236
Oniqqq <Djagawot@gmail.com>
2020-06-13 12:34:26 +0300

    aaa

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -13,7 +13,6 @@ from profiles_api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('profiles_api.urls')),
-    path('login/', views.LoginView.as_view(), name='rest_login'),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
