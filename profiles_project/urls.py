670f57c5db31cef9815ec54db5b84c6762038e66
Oniqqq <Djagawot@gmail.com>
2020-06-14 14:14:19 +0300

    aa

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from allauth.account.views import confirm_email, password_reset, Pas
+from allauth.account.views import confirm_email, password_reset
 from django.conf import settings
 from profiles_api import views
 
@@ -19,9 +19,6 @@ urlpatterns = [
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
     url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        PasswordResetConfirmView.as_view(),
-        name='auth_password_reset_confirm'),
 ]
 
 if settings.DEBUG:
