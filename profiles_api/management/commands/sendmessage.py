efeec69f805043f392a42fcd1ce6b096b9ca1184
Dzhalil <djagawot@gmail.com>
2020-09-08 21:15:18 +0500

    sad

--- profiles_api/management/commands/sendmessage.py
+++ profiles_api/management/commands/sendmessage.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 
         for token in tokens_query:
             device = APNSDevice.objects.get(registration_id=token)
-            device.send_message("You have opened Capsules!", sound='default', categoryIdentifier='0')
+            device.send_message("You have opened Capsules!", sound='default')
 
         for gap in users_query.all():
             gap.notificationsent = True
