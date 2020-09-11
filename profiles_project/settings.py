7bd25c3a938e4b0740bf005e5f10a6033e9f4923
Dzhalil <djagawot@gmail.com>
2020-09-11 12:29:26 +0500

    asd

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -233,7 +233,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 '''
 PUSH_NOTIFICATIONS_SETTINGS = {
