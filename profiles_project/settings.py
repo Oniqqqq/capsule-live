0263ac59ac01232c447484e6148dc37517b381bd
Dzhalil <djagawot@gmail.com>
2020-09-11 15:21:43 +0500

    asd

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's(^ac6+$keq1jp46z-bdx6q%2j_wncz!=x_p9az3xjta9hw&m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.herokussl.com', '*.herokussl.com', '*.herokuapp.com', '.yourtimecapsule.live']
@@ -166,6 +166,8 @@ STATICFILES_DIRS = [
 
 ]
 
+STATIC_HOST = 'https://www.yourtimecapsule.live' if not DEBUG else ''
+STATIC_URL = STATIC_HOST + '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
